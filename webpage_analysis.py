79ef62b3a60750f2181f1852e9a4b3eec90eefcc
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-22 22:58:31 +0200

    Update webpage_analysis.py

--- webpage_analysis.py
+++ webpage_analysis.py
@@ -19,3 +19,18 @@ def analyze_webpage(url, openai):
     new_schema = response.choices[0].text.strip()
 
     return webpage_text, current_schema, new_schema
+
+def merge_schemas(old_schema, new_schema, openai):
+    prompt = f"I have two schema markups from the same webpage. The first one is: {old_schema}. The second one is: {new_schema}. Please merge these two schemas and resolve any conflicts."
+    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=1000)
+    merged_schema = response.choices[0].text.strip()
+
+    return merged_schema
+
+def validate_schema(schema, openai):
+    prompt = f"I have a schema markup: {schema}. Please check if it follows the best practices of schema.org vocabulary and suggest any corrections if needed."
+    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=1000)
+    validation_result = response.choices[0].text.strip()
+
+    return validation_result
+
