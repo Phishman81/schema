a977699f2a06b8ac0ef1097b0586330934817e19
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-22 23:17:54 +0200

    Update webpage_analysis.py
    gpt-3.5-turbo-16k

--- webpage_analysis.py
+++ webpage_analysis.py
@@ -15,22 +15,21 @@ def analyze_webpage(url, openai):
 
     # Create GPT-4 prompt to generate schema markup based on webpage content
     prompt = f"Generate a schema markup for a webpage with the following content: {webpage_text}"
-    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=1000)
+    response = openai.Completion.create(engine="gpt-3.5-turbo-16k", prompt=prompt, max_tokens=1000)
     new_schema = response.choices[0].text.strip()
 
     return webpage_text, current_schema, new_schema
 
 def merge_schemas(old_schema, new_schema, openai):
-    prompt = f"I have two schema markups from the same webpage. The first one is: {old_schema}. The second one is: {new_schema}. Please merge these two schemas and resolve any conflicts."
-    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=1000)
+    prompt = f"Merge the following two schemas:\n\nOld Schema: {old_schema}\nNew Schema: {new_schema}"
+    response = openai.Completion.create(engine="gpt-3.5-turbo-16k", prompt=prompt, max_tokens=1000)
     merged_schema = response.choices[0].text.strip()
 
     return merged_schema
 
 def validate_schema(schema, openai):
-    prompt = f"I have a schema markup: {schema}. Please check if it follows the best practices of schema.org vocabulary and suggest any corrections if needed."
-    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=1000)
+    prompt = f"Validate the following schema: {schema}"
+    response = openai.Completion.create(engine="gpt-3.5-turbo-16k", prompt=prompt, max_tokens=1000)
     validation_result = response.choices[0].text.strip()
 
     return validation_result
-
