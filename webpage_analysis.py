71688962fbb00c68c2d69e0188aa64cb2e2d6970
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-22 23:20:51 +0200

    Update webpage_analysis.py

--- webpage_analysis.py
+++ webpage_analysis.py
@@ -15,21 +15,21 @@ def analyze_webpage(url, openai):
 
     # Create GPT-4 prompt to generate schema markup based on webpage content
     prompt = f"Generate a schema markup for a webpage with the following content: {webpage_text}"
-    response = openai.Completion.create(engine="gpt-3.5-turbo-16k", prompt=prompt, max_tokens=1000)
-    new_schema = response.choices[0].text.strip()
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k", messages=[{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": prompt}], max_tokens=1000)
+    new_schema = response.choices[0].message['content'].strip()
 
     return webpage_text, current_schema, new_schema
 
 def merge_schemas(old_schema, new_schema, openai):
     prompt = f"Merge the following two schemas:\n\nOld Schema: {old_schema}\nNew Schema: {new_schema}"
-    response = openai.Completion.create(engine="gpt-3.5-turbo-16k", prompt=prompt, max_tokens=1000)
-    merged_schema = response.choices[0].text.strip()
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k", messages=[{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": prompt}], max_tokens=1000)
+    merged_schema = response.choices[0].message['content'].strip()
 
     return merged_schema
 
 def validate_schema(schema, openai):
     prompt = f"Validate the following schema: {schema}"
-    response = openai.Completion.create(engine="gpt-3.5-turbo-16k", prompt=prompt, max_tokens=1000)
-    validation_result = response.choices[0].text.strip()
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k", messages=[{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": prompt}], max_tokens=1000)
+    validation_result = response.choices[0].message['content'].strip()
 
     return validation_result
